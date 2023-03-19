6fa7af30fcebe16a042a92b7d30c3fceefffc5a4
ParkYoDo <31908501+ParkYoDo@users.noreply.github.com>
2023-03-20 00:06:56 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,6 +1,5 @@
 function solution(before, after) {
-    var sortBefore = [...before].sort();
-    var sortAfter = [...after].sort();
+
     
-    return Array(before.length).fill(null).filter((_,i)=>sortBefore[i]===sortAfter[i]).length === before.length?1:0;
+    return [...before].sort().join('') === [...after].sort().join('')?1:0;
 }
