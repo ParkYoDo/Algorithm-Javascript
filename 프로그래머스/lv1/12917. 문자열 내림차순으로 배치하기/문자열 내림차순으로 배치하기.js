ba9191465735245c7097992f9978be8fca8fd201
ParkYoDo <31908501+ParkYoDo@users.noreply.github.com>
2023-03-20 12:39:20 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,5 +1,3 @@
 function solution(s) {
-    var Upper = [...s].filter((a)=>a===a.toUpperCase()).sort().reverse();
-    var Lower = [...s].filter((a)=>a===a.toLowerCase()).sort().reverse();
-    return Lower.concat(Upper).join('');
+    return [...s].sort().reverse().join('');
 }
