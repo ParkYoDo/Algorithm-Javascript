c9fd4509f77267e8c54ea91e072888a68d0b299b
ParkYoDo <31908501+ParkYoDo@users.noreply.github.com>
2023-03-21 19:30:42 +0900

    [level 1] Title: 내적, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,7 +1,3 @@
 function solution(a, b) {
-    var sum = 0;
-    for(var i =0;i<a.length;i++){
-        sum += a[i]*b[i];
-    }
-    return sum;
+    return a.reduce((acc,_,i)=> acc+ a[i]*b[i],0);
 }
