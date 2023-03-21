c0c96feb0a91c975d49d7f5f3aa9cb9625970bc6
ParkYoDo <31908501+ParkYoDo@users.noreply.github.com>
2023-03-21 18:28:55 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,13 +1,8 @@
 function solution(num) {
     var count =0;
     while((num!==1 && count<=500)) {
-        if( num%2 ===0){
-            num= num/2;
-            count = count+1;
-        } else {
-            num = num*3+1;
-            count = count+1;
-        }
+        num%2 ===0 ? num= num/2 : num = num*3+1;
+        count += 1;
     }
     
     return count>500?-1:count;
