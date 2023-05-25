a37f4305f066f26a20dc974ac647a886dd7e2874
ParkYoDo <31908501+ParkYoDo@users.noreply.github.com>
2023-05-25 19:06:06 +0900

    [unrated] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    var result = [];
+    
+    for(var i=0; i<my_string.length; i++){
+        result.push(my_string.slice(i));
+    }
+    
+    return result.sort((a,b)=>a>b?1:-1)
+}
