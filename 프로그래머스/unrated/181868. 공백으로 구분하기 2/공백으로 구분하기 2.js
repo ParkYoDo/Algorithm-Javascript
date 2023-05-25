603f67fbae137ce1804ff7fc3f5bfc95382f71e2
ParkYoDo <31908501+ParkYoDo@users.noreply.github.com>
2023-05-26 01:04:48 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,13 +1,3 @@
 function solution(my_string) {
-    var result = '';
-    for(var i=0; i<my_string.length; i++) {
-        if(my_string[i] !==' ') {
-                if(my_string[i-1]===' ') {
-                 result += ' ' + my_string[i]       
-                } else {
-                    result += my_string[i]       
-                }
-            }
-        }
-    return result.split(' ').filter((a)=>a);
+    return my_string.split(' ').filter((a)=>a);
 }
