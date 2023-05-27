f00817c78c500b1b7959e83e0f3d7c812ae4cc93
ParkYoDo <31908501+ParkYoDo@users.noreply.github.com>
2023-05-27 12:16:37 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.42 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,6 +1,5 @@
 function solution(arr, queries) {
-    queries.forEach((a)=>{
-        const [index1, index2] = a;
+    queries.forEach(([index1,index2])=>{
         [arr[index1], arr[index2]] = [arr[index2],arr[index1]];
     })
     return arr;
