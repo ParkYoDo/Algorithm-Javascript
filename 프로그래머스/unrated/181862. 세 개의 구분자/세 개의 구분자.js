3321ea633973a4c64d1a0ae1ba1d9e2d2c062cfc
ParkYoDo <31908501+ParkYoDo@users.noreply.github.com>
2023-05-31 16:38:10 +0900

    [unrated] Title: 세 개의 구분자, Time: 22.09 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,4 +1,7 @@
 function solution(myStr) {
-    var result =  myStr.replaceAll('a',' ').replaceAll('b', ' ').replaceAll('c', ' ').split(' ').filter((a)=>a);
+    // var result =  myStr.replaceAll('a',' ').replaceAll('b', ' ').replaceAll('c', ' ').split(' ').filter((a)=>a);
+    // return result.length? result : ['EMPTY'] ;
+    var result = myStr.split(/[abc]/).filter((a)=>a);
     return result.length? result : ['EMPTY'] ;
+    
 }
